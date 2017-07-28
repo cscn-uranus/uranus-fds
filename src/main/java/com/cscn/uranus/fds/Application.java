7d011c85dd5702dc38808f09faf4e58c615cb16b
tellxp <xiao.peng@live.cn>
2017-07-29 00:04:31 +0800

    添加serverlet支持

--- src/main/java/com/cscn/uranus/fds/Application.java
+++ src/main/java/com/cscn/uranus/fds/Application.java
@@ -7,13 +7,19 @@ import com.cscn.uranus.fds.flowcontrol.entity.FlowcontrolRawMsg;
 import com.cscn.uranus.fds.flowcontrol.repo.FlowcontrolRawMsgRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         
