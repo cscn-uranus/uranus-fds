01747d0ac70aa5d76c459fb53abe7b99e33517fa
tellxp <xiao.peng@live.cn>
2017-07-28 22:57:21 +0800

    删除bash脚本，改成使用war部署

--- gradle/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# ##################################################################
-#             Powered by Ironfo
-# ##################################################################
-#!/bin/sh
-PID=$(cat /home/setupop/sites/uranus-fds/uranus-fds-api.pid)
-kill -9 $PID
